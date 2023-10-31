19165aecf4ea20d4583dfaa1bf761b134a9118c7
Fiedor_ <stachfie@wp.pl>
2023-10-31 19:12:58 +0100

    css i php działają

--- futbol.php
+++ futbol.php
@@ -14,13 +14,22 @@
     </header>
     <div id="mecze">
         <?php
-        //skrypt1
+        $conn = mysqli_connect('localhost', 'root', '', 'egzamin');
+        $q1 = "SELECT zespol1, zespol2,wynik,data_rozgrywki FROM rozgrywka WHERE zespol1='EVG'";
+        $result = mysqli_query($conn, $q1);
+        foreach ($result as $chunk) {
+            echo '<div class="chunk">';
+            echo "<h3>" . $chunk['zespol1'] . "-" . $chunk['zespol2'] . "</h3>";
+            echo "<h4>" . $chunk['wynik'] . "</h4>";
+            echo "<p> w dniu:" . $chunk['data_rozgrywki'] . "</p>";
+            echo '</div>';
+        }
         ?>
     </div>
+    <div id='glowny'>
+        <h2>Reprezentacja Polski</h2>
+    </div>
     <main>
-        <div id='glowny'>
-            <h2>Reprezentacja Polski</h2>
-        </div>
         <div id='left'>
             <p>Podaj pozycję zawodników (1-bramkarze, 2-obrońcy,3-pomocnicy,4-napastnicy):</p>
             <form action="futbol.php" method="post">
@@ -29,7 +38,14 @@
             </form>
             <ul>
                 <?php
-                //skrypt2
+                $numer = $_POST['numer'];
+                if (isset($_POST['numer'])) {
+                    $q2 = "SELECT imie,nazwisko FROM zawodnik WHERE pozycja_id=$numer";
+                    $result1 = mysqli_query($conn, $q2);
+                    foreach ($result1 as $row) {
+                        echo "<li>" . $row['imie'] . '&nbsp' . $row['nazwisko'] . "</li>";
+                    }
+                }
                 ?>
             </ul>
         </div>
